0f128ce513beff11a389430c0ff25ffebf217671
fillooow <fil201121@yandex.ru>
2021-01-29 23:37:49 +0500

    закинул основные файлы (разметка + вм + фрагмент)

---
+++ app/src/main/java/com/fillooow/shadrin/content/ContentFragment.kt
@@ -0,0 +1,25 @@
+package com.fillooow.shadrin.content
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.fillooow.shadrin.R
+import com.fillooow.shadrin.databinding.ContentFragmentBinding
+
+class ContentFragment : Fragment(R.layout.content_fragment) {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+
+        val binding: ContentFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.content_fragment, container, false)
+
+        binding.vm = ViewModelProvider(this).get(ContentViewModel::class.java)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/fillooow/shadrin/content/ContentViewModel.kt
@@ -0,0 +1,12 @@
+package com.fillooow.shadrin.content
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ContentViewModel : ViewModel() {
+
+    private val _lastName = MutableLiveData("Ada")
+
+    val lastName: LiveData<String> = _lastName
+}
