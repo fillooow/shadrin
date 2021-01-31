20a16954315e4bdaf2b2ff152b3d6d509a3842b1
fillooow <fil201121@yandex.ru>
2021-02-01 01:59:04 +0500

    дописал логику впопыхах

--- app/src/main/java/com/fillooow/shadrin/ui/BindingAdapters.kt
+++ app/src/main/java/com/fillooow/shadrin/ui/BindingAdapters.kt
@@ -1,13 +1,11 @@
 package com.fillooow.shadrin.ui
 
-import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.fillooow.shadrin.R
-import com.fillooow.shadrin.ui.content.LoadState
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -22,22 +20,5 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
                     .error(R.drawable.ic_broken_image)
             )
             .into(imgView)
-    }
-}
-
-@BindingAdapter("loadState")
-fun bindStatus(statusImageView: ImageView, state: LoadState?) {
-    when (state) {
-        LoadState.LOADING -> {
-            statusImageView.visibility = View.VISIBLE
-            statusImageView.setImageResource(R.drawable.loading_animation)
-        }
-        LoadState.ERROR -> {
-            statusImageView.visibility = View.VISIBLE
-            statusImageView.setImageResource(R.drawable.ic_connection_error)
-        }
-        LoadState.DONE -> {
-            statusImageView.visibility = View.GONE
-        }
     }
 }
--- app/src/main/java/com/fillooow/shadrin/ui/content/ContentViewModel.kt
+++ app/src/main/java/com/fillooow/shadrin/ui/content/ContentViewModel.kt
@@ -6,47 +6,54 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fillooow.shadrin.network.DevelopersLifeApi
 import com.fillooow.shadrin.network.DevelopersLifeProperty
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class ContentViewModel : ViewModel() {
 
-    init {
-        getProperty()
-    }
-
     private val responseCache = MutableLiveData<MutableList<DevelopersLifeProperty>>()
     val currentItem = MutableLiveData<DevelopersLifeProperty>()
+    val canClickBack = MutableLiveData<Boolean>()
+    private var currentPosition = -1
 
-    val status = MutableLiveData<DevelopersLifeProperty>()
-
-    private fun getMarsRealEstateProperties() {
-
-        viewModelScope.launch {
-//            _status.value = MarsApiStatus.LOADING
-            try {
-//                _properties.value = MarsApi.retrofitService.getProperties()
-//                _status.value = MarsApiStatus.DONE
-            } catch (e: Exception) {
-//                _status.value = MarsApiStatus.ERROR
-//                _properties.value = ArrayList()
-            }
-        }
+    init {
+        responseCache.value = mutableListOf()
+        getProperty()
     }
 
     private fun getProperty() {
 
         viewModelScope.launch {
             try {
-                val result = DevelopersLifeApi.retrofitService.getRandomPost()
-                currentItem.value = result
-                responseCache.value?.add(result)
+                val result = async { DevelopersLifeApi.retrofitService.getRandomPost() }
+                currentItem.value = result.await()
+
+                canClickBack.value = (responseCache.value?.size ?: 0 - 1) > 0
             } catch (e: Exception) {
                 Log.e("error", "Failure: ${e.message}")
+            } finally {
+
+                responseCache.value?.add(currentItem.value!!)
             }
         }
     }
 
-    fun onClick() = ::getProperty
+    fun onNextClick() {
+
+        currentPosition++
+        if (currentPosition == responseCache.value?.size ?: 0 - 1) { // не успел доделать проверку на выход за границы массива
+            getProperty()
+        } else {
+            currentItem.value = responseCache.value?.get(currentPosition)
+        }
+
+    }
+
+    fun onPrevClick() {
+
+        currentPosition--
+        currentItem.value = responseCache.value?.get(currentPosition)
+    }
 }
 
-enum class LoadState { LOADING, ERROR, DONE }
+// все выходные был занят и посидел над задачкой всего лишь пару часов :(
